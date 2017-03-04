2058759ecef075085c4d5896f8b1b9bbfdb1a214
taufiqur rahman <taufiqur@cukuptauaja.com>
2017-03-04 14:08:43 +0700

    Perbaharui log pdf lagi

---
+++ Kode Latihan/Pertemuan 3/Kode1.js
@@ -0,0 +1,11 @@
+// Deklarasi fungsi
+ 
+function nama_function(){
+   // isi function nya di buat di sini
+}
+ 
+// Deklarasi fungsi dengan argumen
+ 
+function nama_function(argumen1, argumen2, dst){
+   // isi function nya di buat di sini
+}
---
+++ Kode Latihan/Pertemuan 3/Kode3.js
@@ -0,0 +1,12 @@
+// contoh deklarasi fungsinya
+
+var tambah = function (a, b) {
+    var hasil = a + b;
+    return hasil;
+};
+
+
+
+// contoh pemanggilan fungsinya dengan memasukan argumen 3, dan 5
+
+tambah(3, 5);
